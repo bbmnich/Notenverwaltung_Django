25b40602a3d4916c9deb80937378dc6fdea5ecef
Barbara <bbmnich@aol.com>
2026-07-30 15:55:09 +0200

    UI für Studenten-Eingabe erstellt und Duplikate/Leerzeichen  hinzugefügt

--- basis/models.py
+++ basis/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+# Student
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+# Kurs
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    max_score = models.IntegerField()
+
+    def __str__(self):
+        return self.name + " (Max: " + str(self.max_score) + ")"
+
+# Noten
+class Grade(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    score = models.IntegerField()
+
+    def __str__(self):
+        return self.student.name + " - " + self.course.name + ": " + str(self.score) + " Punkte"
--- basis/views.py
+++ basis/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from .models import Student
 
-# Create your views here.
+def startseite(request):
+    # Leerer Standardwert, falls kein neuer Student gespeichert wird.
+    meldung = ""
+
+    if request.method == "POST":
+        # Hat der Nutzer Daten gesendet? 2. Rohen Text aus dem Feld holen. 3. Text mit .strip() bereinigen.
+        roher_name = request.POST.get("eingabe_name")
+        neuer_name = roher_name.strip()
+        
+        # Leerzeichen Prüfung
+        if neuer_name == "":
+            meldung = "Fehler: Der Name darf nicht leer sein."
+        else:
+            # Duplikate verhindern
+            student_existiert = Student.objects.filter(name=neuer_name).exists()
+            
+            if student_existiert:
+                meldung = "Fehler: Der Student " + neuer_name + " existiert bereits!"
+            else:
+                # Nur wenn alle Prüfungen Ok wird gespeichert
+                neuer_student = Student(name=neuer_name)
+                neuer_student.save()
+                meldung = "Erfolg: " + neuer_name + " wurde angelegt!"
+
+    # aktualisierte Liste aus der Datenbank holen
+    alle_studenten = Student.objects.all()
+
+    # Studenten UND die neue Meldung anzeigen
+    return render(request, "startseite.html", {"alle_studenten": alle_studenten, "meldung": meldung})
